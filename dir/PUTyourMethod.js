420a4a56d68688b41fcdcf14a201a0ffebe18c3d
Marcin <mkulwikowski@ibt.pt>
2014-12-05 13:25:27 +0000

    gitManager

--- dir/PUTyourMethod.js
+++ dir/PUTyourMethod.js
@@ -1,4 +1,4 @@
 function yourMethod(teste,res){
 
-	res.send(202, "PUT", {'Access-Control-Allow-Origin': 'http://example.com:8080'/*,"Access-Control-Allow-Headers": "X-Requested-With"*/});
+	res.send(202, "PUT", {/*'Access-Control-Allow-Origin': 'http://example.com:8080'/*,"Access-Control-Allow-Headers": "X-Requested-With"*/});
 }
